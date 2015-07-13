9dafa78f24b346f319dbd9f1de2f2d29ece08cc5
Alessandro Petrelli <alessandro@appspace.it>
2015-07-13 18:51:31 +0200

    add test utility for mongodb

---
+++ src/RedCapped.Core.Tests/MongoDbUtils.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Driver;
+
+namespace RedCapped.Core.Tests
+{
+    public static class MongoDbUtils
+    {
+        public const string ConnectionString = "mongodb://localhost";
+        public const string DatabaseName = "redcappedtest";
+
+        public static void DropDatabase()
+        {
+            var client = new MongoClient(ConnectionString);
+            client.DropDatabaseAsync(DatabaseName).Wait();
+        }
+    }
+}
