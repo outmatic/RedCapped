384ef2154ae9d8cad605d4770b265e327bf00e27
Alessandro Petrelli <alessandro@appspace.it>
2016-01-31 20:12:57 +0100

    promoted to 1.0 version

--- src/RedCapped.Core.Tests/Properties/AssemblyInfo.cs
+++ src/RedCapped.Core.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RedCapped.Core.Tests")]
-[assembly: AssemblyCopyright("Copyright © Alessandro Petrelli 2015")]
+[assembly: AssemblyCopyright("Copyright © Alessandro Petrelli 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.0.0.*")]
+[assembly: AssemblyVersion("1.0.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
